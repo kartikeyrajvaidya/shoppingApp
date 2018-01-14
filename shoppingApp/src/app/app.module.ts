1e95beadde2bed6f79ce58dbc279af72fa90abc9
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-14 12:09:51 +0530

    added menu-card component

--- shoppingApp/src/app/app.module.ts
+++ shoppingApp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SingUpPageComponent } from './sing-up-page/sing-up-page.component';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
     LoginPageComponent,
-    SingUpPageComponent
+    SingUpPageComponent,
+    MenuCardComponent
   ],
   imports: [
     BrowserModule
