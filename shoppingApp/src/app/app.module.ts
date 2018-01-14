0824e5fa5ba407d2c3643ea2501908df9b785813
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-14 12:01:16 +0530

    added singUp-page component

--- shoppingApp/src/app/app.module.ts
+++ shoppingApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SingUpPageComponent } from './sing-up-page/sing-up-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SingUpPageComponent
   ],
   imports: [
     BrowserModule
