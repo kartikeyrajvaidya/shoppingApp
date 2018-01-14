1f8eeb55e7af784d5b3d06a19c29ce004faff747
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-14 12:00:07 +0530

    added login-page component

--- shoppingApp/src/app/app.module.ts
+++ shoppingApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
