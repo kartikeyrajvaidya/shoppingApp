e793d00d3000e582fdddcb4f34c3b04de23456c0
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-14 11:58:28 +0530

    added shopping-cart component

--- shoppingApp/src/app/app.module.ts
+++ shoppingApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
