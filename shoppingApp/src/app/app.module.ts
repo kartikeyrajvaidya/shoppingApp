5d0ef982b4e7289a03c19ba15bf5dd1733831b1e
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-23 14:23:10 +0530

    added Header component: Kartikey

--- shoppingApp/src/app/app.module.ts
+++ shoppingApp/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SingUpPageComponent } from './sing-up-page/sing-up-page.component';
-import { AppRouting } from "./app-routing.module";
+import { AppRouting } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     SingUpPageComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule,AppRouting
+    BrowserModule, AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
